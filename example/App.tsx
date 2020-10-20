cea1677064c873a32f7cf5dbf9911f470f177e64
FreakyCoder <kurayogun@gmail.com>
2020-10-20 23:42:31 +0300

    Example is updated

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StatusBar, SafeAreaView } from "react-native";
-import RNAnimated from "./build/dist/RNAnimated";
+import RNAnimated from "react-native-animated-component";
 
 const staticData = [1, 2, 3, 4, 5, 6, 7, 1, 1, 4, 5, 31];
 
